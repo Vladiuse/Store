cea44a6d1959f80382e4ee0a00b4734f466128d5
vladiuse <vladiuse@gmail.com>
2023-06-07 12:08:31 +0300

    add swagger doc

--- HelloDjango/HelloDjango/settings.py
+++ HelloDjango/HelloDjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #apps
     'rest_framework',
+    'drf_yasg',
     'corsheaders',
     'buttons.apps.ButtonsConfig'
 ]
--- HelloDjango/buttons/views.py
+++ HelloDjango/buttons/views.py
@@ -7,6 +7,7 @@ from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Book, Genre
+from rest_framework import permissions
 
 
 def index(request):
@@ -34,3 +35,4 @@ class BookViewSet(ModelViewSet):
 class GenreViewSet(ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
