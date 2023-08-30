6e04476d4012daac1238e40f7d0ae7fcd40b2769
vladiuse <vladiuse@gmail.com>
2023-08-30 23:20:51 +0300

    add few tests

--- HelloDjango/user_api/tests/test_user_model.py
+++ HelloDjango/user_api/tests/test_user_model.py
@@ -4,16 +4,25 @@ from rest_framework.reverse import reverse
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from user_api.models import Profile, Employee, Position
+from faker import Faker
+
+f = Faker()
 
 User = get_user_model()
 
+def create_user():
+    return User.objects.create_user(username=f.first_name(), password='0000')
+
+def create_employee_user():
+    user = User.objects.create_user(username=f.first_name(), password='0000')
+    position = Position.objects.create(id=f.first_name(), name=f.first_name())
+    employee = Employee.objects.create(position=position, user=user)
+    return employee, user
 
 class UserTestCase(APITestCase):
 
     def test_is_employee(self):
-        user = User.objects.create_user(username='test', password='0000')
-        position = Position.objects.create(id='xxx', name='xxx')
-        employee = Employee.objects.create(position=position, user=user)
+        employee, user = create_employee_user()
         self.assertTrue(user.is_employee)
 
     def test_is_not_employee(self):
