bb1c9d18ad5460e41657a7eacb778b82c117e663
vladiuse <vladiuse@gmail.com>
2023-08-30 23:20:47 +0300

    add few tests

--- HelloDjango/user_api/tests/test_user_view.py
+++ HelloDjango/user_api/tests/test_user_view.py
@@ -5,11 +5,12 @@ from rest_framework.reverse import reverse
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from user_api.models import Profile, Employee, Position
+from .test_user_model import create_employee_user, create_user
 
 User = get_user_model()
 
 
-class UserViewTestCase(APITestCase):
+class UserView(APITestCase):
 
     def setUp(self) -> None:
         #
@@ -55,7 +56,6 @@ class UserViewTestCase(APITestCase):
         res = self.client.post(login_url, data=data, format='json')
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-
     def test_create_user_registration(self):
         register_url = reverse('register')
         data = {
@@ -65,3 +65,55 @@ class UserViewTestCase(APITestCase):
         res = self.client.post(register_url, data=data, format='json')
         self.assertEqual(res.status_code, 200)
         self.assertEqual(User.objects.count(), 2)
+
+
+class UserViewList(APITestCase):
+    def test_get_users_list_not_auth(self):
+        user_list_url = reverse('myuser-list')
+        res = self.client.get(user_list_url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_user_list_not_employee(self):
+        user = create_user()
+        self.client.force_login(user=user)
+        user_list_url = reverse('myuser-list')
+        res = self.client.get(user_list_url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_users_list_employee_user(self):
+        employee, user = create_employee_user()
+        self.client.force_login(user=user)
+        user_list_url = reverse('myuser-list')
+        res = self.client.get(user_list_url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class UserViewDetail(APITestCase):
+
+    def test_not_auth(self):
+        user = create_user()
+        user_detail_url = reverse('myuser-detail', args=[user.pk, ])
+        res = self.client.get(user_detail_url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_auth_not_employee(self):
+        user = create_user()
+        self.client.force_login(user=user)
+        user_detail_url = reverse('myuser-detail', args=[user.pk, ])
+        res = self.client.get(user_detail_url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_employee_show_self(self):
+        employee, user = create_employee_user()
+        self.client.force_login(user=user)
+        user_detail_url = reverse('myuser-detail', args=[user.pk, ])
+        res = self.client.get(user_detail_url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_user_employee_other_user_detail(self):
+        employee, user = create_employee_user()
+        user_1 = create_user()
+        self.client.force_login(user=user)
+        user_detail_url = reverse('myuser-detail', args=[user_1.pk, ])
+        res = self.client.get(user_detail_url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
