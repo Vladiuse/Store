a63b67c16b80fae86436869c3a6abb8f2f8e2eba
vladiuse <vladiuse@gmail.com>
2023-08-30 23:20:04 +0300

    remove some permisions from store app in user_api add IsEmployee perms

---
+++ HelloDjango/user_api/permisions.py
@@ -0,0 +1,39 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.contrib.auth.models import Group
+
+class IsOwnerPermissions(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return request.user and request.user == obj.owner
+
+
+class IsOwnerPermissionsSafe(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user == obj.owner
+
+
+class IsEmployee(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_employee
+
+
+class _IsGroupPermissions(BasePermission):
+    GROUP_NAME = ''
+
+    def has_object_permission(self, request, view, obj):
+        group = Group.objects.get(name=self.GROUP_NAME)
+        return request.user.groups.contains(group)
+
+
+class IsModeratorPermissions(_IsGroupPermissions):
+    GROUP_NAME = 'moderator'
+
+class IsManagerPermissions(_IsGroupPermissions):
+    GROUP_NAME = 'manager'
+
+class IsSellerPermissions(_IsGroupPermissions):
+    GROUP_NAME = 'seller'
