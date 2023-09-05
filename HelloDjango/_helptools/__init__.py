a060345064dc1fd6de5aeec68e865910499532d8
vladiuse <vladiuse@gmail.com>
2023-09-05 17:32:12 +0300

    add create group in create_employee func

--- HelloDjango/_helptools/__init__.py
+++ HelloDjango/_helptools/__init__.py
@@ -2,6 +2,7 @@ import random as r
 import string
 from user_api.models import Profile, Employee, Position
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from faker import Faker
 
 f = Faker()
@@ -18,8 +19,11 @@ def create_user():
     return User.objects.create_user(username=f.first_name() + str_random(3), password='0000')
 
 
-def create_employee_user():
+def create_employee_user(add_group=False):
     user = User.objects.create_user(username=f.first_name() + str_random(3), password='0000')
     position = Position.objects.create(id=f.first_name(), name=f.first_name())
     employee = Employee.objects.create(position=position, user=user)
+    if add_group:
+        group = Group.objects.create(name=add_group)
+        user.groups.add(group)
     return employee, user
