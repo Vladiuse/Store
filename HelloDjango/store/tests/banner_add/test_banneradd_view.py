0b9ff1dea4ab713f40174c31642ae597a0cf5378
vladiuse <vladiuse@gmail.com>
2023-09-11 18:21:55 +0300

    add few tests

--- HelloDjango/store/tests/banner_add/test_banneradd_view.py
+++ HelloDjango/store/tests/banner_add/test_banneradd_view.py
@@ -81,3 +81,123 @@ class BannerAddViewCreateTest(APITestCase):
             res = self.client.post(self.banner_url, data=data, format='multipart')
             self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual(BannerAdd.objects.count(), 3)
+
+
+class BannerAddViewUpdateTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.banner = create_banner()
+        self.banner_url = reverse('banners-detail', args=[self.banner.pk, ])
+
+        Group.objects.create(name='moderator')
+
+    def test_no_auth(self):
+        res = self.client.put(self.banner_url, data={}, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        res = self.client.patch(self.banner_url, data={}, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user(self):
+        user = create_user()
+        self.client.force_login(user=user)
+        res = self.client.put(self.banner_url, data={}, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        res = self.client.patch(self.banner_url, data={}, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_employee(self):
+        employee, emp_user = create_employee_user()
+        self.client.force_login(user=emp_user)
+        res = self.client.put(self.banner_url, data={}, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        res = self.client.patch(self.banner_url, data={}, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_employee_moderator(self):
+        employee, emp_user = create_employee_user(add_group='moderator')
+        self.client.force_login(user=emp_user)
+        data = banner_fake_data(json=True, title='xxx', order=1)
+        res = self.client.put(self.banner_url, data=data, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        banner = BannerAdd.objects.get(pk=self.banner.pk)
+        self.assertEqual(banner.title, 'xxx')
+
+        data = {
+            'title': 'yyy',
+        }
+        res = self.client.patch(self.banner_url, data=data, format='multipart')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        banner = BannerAdd.objects.get(pk=self.banner.pk)
+        self.assertEqual(banner.title, 'yyy')
+
+
+class BannerAddViewDeleteTest(APITestCase):
+
+    def setUp(self) -> None:
+
+        Group.objects.create(name='moderator')
+        self.banner = create_banner()
+        self.banner_url = reverse('banners-detail', args=[self.banner.pk,])
+
+    def test_no_auth(self):
+        res = self.client.delete(self.banner_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_auth(self):
+        user = create_user()
+        self.client.force_login(user=user)
+        res = self.client.delete(self.banner_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_auth_employee(self):
+        employee, emp_user = create_employee_user()
+        self.client.force_login(user=emp_user)
+        res = self.client.delete(self.banner_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_auth_employee_moderator(self):
+        employee, emp_moderator = create_employee_user(add_group='moderator')
+        self.client.force_login(user=emp_moderator)
+        res = self.client.delete(self.banner_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+class BannerAddViewQuerySetTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.banners_url = reverse('banners-list')
+        for _ in range(3):
+            create_banner()
+
+        add = BannerAdd.objects.last()
+        add.is_public = False
+        add.save()
+
+    def test_no_auth(self):
+        res = self.client.get(self.banners_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+
+    def test_auth(self):
+        user = create_user()
+        self.client.force_login(user=user)
+        res = self.client.get(self.banners_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+
+    def test_employee(self):
+        employee, emp_user = create_employee_user()
+        self.client.force_login(user=emp_user)
+        res = self.client.get(self.banners_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+
+    def test_employee_moderator(self):
+        employee, emp_user = create_employee_user(add_group='moderator')
+        self.client.force_login(user=emp_user)
+        res = self.client.get(self.banners_url, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 3)
+
