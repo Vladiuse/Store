e6a9309e2f88b14c2998f67e94920dc8d51b51e8
vladiuse <vladiuse@gmail.com>
2023-09-13 18:59:41 +0300

    add Order model

--- HelloDjango/orders/models.py
+++ HelloDjango/orders/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from store.models import Basket
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Order(models.Model):
+    CREATED = 0
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+        (CREATED, 'Создан'),
+        (PAID, 'Оплачен'),
+        (ON_WAY, 'В пути'),
+        (DELIVERED, 'Доставлен'),
+    )
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    phone = models.CharField(
+        max_length=13,
+    )
+    email = models.EmailField(
+        blank=True,
+    )
+    status = models.SmallIntegerField(
+        choices=STATUSES,
+        default=CREATED,
+    )
+    basket_history = models.JSONField()
